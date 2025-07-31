30b3e9d78f1fb678676329ff6ca8749fe98688ac
muhammad-saad92 <mughalll92@gmail.com>
2025-07-31 20:35:57 +0500

    added changes

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 let deckId = '';
 
-fetch('https://deckofcardsapi.com/api/deck/atakr5asehnx/draw/?count=2')
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 .then(res => res.json())
 .then(data => {
     console.log(data)
     deckId = data.deck_id;
+    console.log(deckId);
 })
 .catch(err => {
     console.log(`Error: ${err}`)
 })
 
+document.querySelector('button').addEventListener('click',drawTwo)
+
+function drawTwo(){
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(`error: ${err}`)
+    })
+}
 
 
